4b0fd37db2be67342abf9d510a97d40e2067f5e3
Andy Tomlinson <143251083+andytomlinson123@users.noreply.github.com>
2024-02-10 21:27:12 +0000

    deployVpn

---
+++ 009-deployVpn/deployVpnPt1.ps1
@@ -0,0 +1,115 @@
+Write-Host "Creating variables"
+$resourceGroupDcName = "rg-learn-dc"
+$resourceGroupLocation = "uksouth"
+$virtualNetworkDcName = "vnet-learn-dc"
+$virtualNetworkDcAddress = "10.127.0.0/16"
+$gatewaySubnetName = "GatewaySubnet"
+$gatewaySubnetAddress = "10.127.0.0/24"
+$virtualNetworkGatewayName = "gw-learn-dc"
+$virtualNetworkGatewayIpName = "$virtualNetworkGatewayName-ip"
+$subnetDcName = "snet-learn-dc"
+$subnetDcNsgName = "nsg-$subnetDcName"
+$subnetDcAddress = "10.127.1.0/24"
+$virtualMachineDcName = "vm-learn-dc"
+$virtualMachineDcNicName = "$virtualMachineDcName-nic"
+$virtualMachineDcDiskName = "$virtualMachineDcName-disk-os"
+$virtualMachineSize = "Standard_B1ls"
+$virtualMachineImage = "Ubuntu2204"
+$virtualMachineUsername = "learnadmin"
+$virtualMachinePassword = "ReplaceMe24!"
+
+Write-Host "Creating resource group: $resourceGroupDcName"
+az group create `
+  --name $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating virtual network: $virtualNetworkDcName"
+az network vnet create `
+  --name $virtualNetworkDcName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --address-prefixes $virtualNetworkDcAddress `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating subnet: $gatewaySubnetName"
+az network vnet subnet create `
+  --name $gatewaySubnetName `
+  --resource-group $resourceGroupDcName `
+  --vnet-name $virtualNetworkDcName `
+  --address-prefixes $gatewaySubnetAddress `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating network security group: $subnetDcNsgName"
+az network nsg create `
+  --name $subnetDcNsgName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating subnet: $subnetDcName"
+az network vnet subnet create `
+  --name $subnetDcName `
+  --resource-group $resourceGroupDcName `
+  --vnet-name $virtualNetworkDcName `
+  --address-prefixes $subnetDcAddress `
+  --network-security-group $subnetDcNsgName `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating public IP address: $virtualNetworkGatewayIpName"
+az network public-ip create `
+  --name $virtualNetworkGatewayIpName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --sku Standard `
+  --allocation-method Static `
+  --only-show-errors `
+  --output None
+    
+Write-Host "Creating virtual network gateway: $virtualNetworkGatewayName"
+az network vnet-gateway create `
+  --name $virtualNetworkGatewayName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --sku VpnGw1 `
+  --gateway-type Vpn `
+  --vpn-gateway-generation Generation1 `
+  --vpn-type RouteBased `
+  --vnet $virtualNetworkDcName `
+  --public-ip-address $virtualNetworkGatewayIpName `
+  --no-wait `
+  --only-show-errors `
+  --output None
+
+Write-Host "Create network interface: $virtualMachineDcNicName"
+az network nic create `
+  --name $virtualMachineDcNicName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --vnet-name $virtualNetworkDcName `
+  --subnet $subnetDcName `
+  --only-show-errors `
+  --output None
+  
+Write-Host "Creating virtual machine: $virtualMachineDcName"
+az vm create `
+  --name $virtualMachineDcName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --size $virtualMachineSize `
+  --admin-username $virtualMachineUsername `
+  --admin-password $virtualMachinePassword `
+  --image $virtualMachineImage `
+  --os-disk-name $virtualMachineDcDiskName `
+  --storage-sku StandardSSD_LRS `
+  --nics $virtualMachineDcNicName `
+  --no-wait `
+  --only-show-errors `
+  --output None
+
+Write-Host "Deployment complete"
---
+++ 009-deployVpn/deployVpnPt2.ps1
@@ -0,0 +1,80 @@
+Write-Host "Creating variables"
+$resourceGroupHubName = "rg-learn-hub"
+$resourceGroupLocation = "uksouth"
+$virtualNetworkGatewayHubName = "gw-learn-hub"
+$virtualNetworkGatewayHubIpName = "$virtualNetworkGatewayHubName-ip"
+$localNetworkGatewayHubName = "lng-learn-hub"
+$virtualNetworkHubAddress = "10.0.0.0/16"
+$virtualNetwork01Address = "10.1.0.0/16"
+$virtualNetwork02Address = "10.2.0.0/16"
+$virtualNetwork03Address = "10.3.0.0/16"   
+$virtualNetworkGatewayHubName = "gw-learn-hub"
+$resourceGroupDcName = "rg-learn-dc"
+$virtualNetworkGatewayDcName = "gw-learn-dc"
+$virtualNetworkGatewayDcIpName = "$virtualNetworkGatewayDcName-ip"
+$localNetworkGatewayDcName = "lng-learn-dc"
+$virtualNetworkDcAddress = "10.127.0.0/16"
+$virtualNetworkGatewayDcName = "gw-learn-dc"
+$connectionNameHub = "con-$virtualNetworkGatewayDcName"
+$connectionNameDc = "con-$virtualNetworkGatewayHubName"
+$connectionPreSharedKey = "ReplaceMe24!"
+
+Write-Host "Retrieving public IP address: $virtualNetworkGatewayHubIpName"
+$virtualNetworkGatewayHubIp = az network public-ip show `
+  --name $virtualNetworkGatewayHubIpName `
+  --resource-group $resourceGroupHubName `
+  --only-show-errors `
+  --query ipAddress `
+  --output tsv
+
+Write-Host "Retrieving public IP address: $virtualNetworkGatewayDcIpName"
+$virtualNetworkGatewayDcIp = az network public-ip show `
+  --name $virtualNetworkGatewayDcIpName `
+  --resource-group $resourceGroupDcName `
+  --only-show-errors `
+  --query ipAddress `
+  --output tsv
+
+Write-Host "Creating local network gateway: $localNetworkGatewayHubName"
+az network local-gateway create `
+  --name $localNetworkGatewayHubName `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --gateway-ip-address $virtualNetworkGatewayHubIp `
+  --address-prefixes $virtualNetworkHubAddress $virtualNetwork01Address $virtualNetwork02Address $virtualNetwork03Address `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating local network gateway: $localNetworkGatewayDcName"
+az network local-gateway create `
+  --name $localNetworkGatewayDcName `
+  --resource-group $resourceGroupHubName `
+  --location $resourceGroupLocation `
+  --gateway-ip-address $virtualNetworkGatewayDcIp `
+  --address-prefixes $virtualNetworkDcAddress `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating site to site connection: $connectionNameHub"
+az network vpn-connection create `
+  --name $connectionNameHub `
+  --resource-group $resourceGroupHubName `
+  --location $resourceGroupLocation `
+  --vnet-gateway1 $virtualNetworkGatewayHubName `
+  --local-gateway2 $localNetworkGatewayDcName `
+  --shared-key $connectionPreSharedKey `
+  --only-show-errors `
+  --output None
+
+Write-Host "Creating site to site connection: $connectionNameDc"
+az network vpn-connection create `
+  --name $connectionNameDc `
+  --resource-group $resourceGroupDcName `
+  --location $resourceGroupLocation `
+  --vnet-gateway1 $virtualNetworkGatewayDcName `
+  --local-gateway2 $localNetworkGatewayHubName `
+  --shared-key $connectionPreSharedKey `
+  --only-show-errors `
+  --output None
+
+Write-Host "Deployment complete"
